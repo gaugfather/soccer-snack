bbf9fabe42386fe8d5b851fa20f7c5573ca8ba16
gaugfather <derek.gauger@gmail.com>
2021-08-16 10:57:14 -0500

    migrate to react

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './snacksignup.css';
 import './index.css';
-import App from './App';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import App from './components/app/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
